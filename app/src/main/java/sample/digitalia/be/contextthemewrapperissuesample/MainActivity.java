b9dc413ae8e94942a69683aee3949ddfe2e646ba
Christophe Beyls <software@digitalia.be>
2019-05-12 13:22:17 +0200

    Upgrade to the latest build tools, AndroidX and AppCompat 1.1.0-alpha05

--- app/src/main/java/sample/digitalia/be/contextthemewrapperissuesample/MainActivity.java
+++ app/src/main/java/sample/digitalia/be/contextthemewrapperissuesample/MainActivity.java
@@ -1,23 +1,23 @@
 package sample.digitalia.be.contextthemewrapperissuesample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		// Custom inner layout inflation with a dark theme
-		ViewGroup container = (ViewGroup) findViewById(R.id.inner_layout_container);
+        // Custom inner layout inflation with a dark theme
+        ViewGroup container = findViewById(R.id.inner_layout_container);
 
-		ContextThemeWrapper wrapper = new ContextThemeWrapper(this, R.style.ThemeOverlay_AppCompat_Dark);
-		LayoutInflater.from(wrapper).inflate(R.layout.inner_layout, container, true);
-	}
+        ContextThemeWrapper wrapper = new ContextThemeWrapper(this, R.style.ThemeOverlay_AppCompat_Dark);
+        LayoutInflater.from(wrapper).inflate(R.layout.inner_layout, container, true);
+    }
 }
